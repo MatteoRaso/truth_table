e52eb6d72c5dfdc6687a4f8b277bd8ffa242fda3
Matteo Raso <mraso@uoguelph.ca>
2020-10-06 21:10:00 -0400

    Added xor function.

---
+++ operators/xor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def xor(q, r):
+    if q and r:
+        answer = False
+
+    elif not q and not r:
+        answer = False
+
+    else:
+        answer = True
+
+    return answer
